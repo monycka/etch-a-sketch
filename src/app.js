7b0a71cbae528e7cfb6b93ebf89f39d9b71cc4f4
monycka <monyckadc@gmail.com>
2021-11-24 21:01:01 -0500

    Added buttons

--- src/app.js
+++ src/app.js
@@ -1,13 +1,5 @@
-const container = document.getElementById("container");
-
-function makeRows(rows, cols) {
-  container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-cols', cols);
-  for (c = 0; c < (rows * cols); c++) {
-    let cell = document.createElement("div");
-    cell.innerText = (c + 1);
-    container.appendChild(cell).className = "grid-item";
-  };
-};
-
-makeRows(16, 16);
+function removeActiveStyle(buttons) {
+  buttons.forEach((button) => {
+    button.classList.remove('active');
+  });
+}
