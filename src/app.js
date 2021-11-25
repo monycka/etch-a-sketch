4cc905eb94aad2b65ca6caa722a4566f25a3e20e
monycka <monyckadc@gmail.com>
2021-11-24 21:28:33 -0500

    Added colors to JS

--- src/app.js
+++ src/app.js
@@ -3,3 +3,23 @@ function removeActiveStyle(buttons) {
     button.classList.remove('active');
   });
 }
+
+
+
+function generateColor(name, colors) {
+  const gridItem = document.querySelectorAll('.grid-container > div');
+  gridItem.forEach((item) => {
+    if (name === 'warm' || name === 'cold') {
+      const randomColors = colors[Math.floor(
+        Math.random() * colors.length,
+      )];
+      item.addEventListener('mouseenter', (e) => {
+        e.target.style.backgroundColor = randomColors;
+      });
+    } else if (name === 'black' || name === 'white') {
+      item.addEventListener('mouseenter', (e) => {
+        e.target.style.backgroundColor = colors;
+      });
+    }
+  });
+}
